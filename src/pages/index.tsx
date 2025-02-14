aa6bbb64179abd7ff137b292b0f78d28ce1bffa4
dndangg <144980612+dndangg@users.noreply.github.com>
2025-02-13 23:00:17 -0600

    Set single background color for home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Home: React.FC = () => {
   return (
-    <div className="bg-[#808977] flex items-center justify-center min-h-screen">
+    <div className="fixed inset-0 flex items-center justify-center bg-[#808977]">
       {/* Main Content */}
       <div className="bg-[#f3e6d5] p-16 rounded-lg shadow-lg w-[95%] max-w-4xl border border-[#554f42]">
         <h1 className="text-center text-3xl font-bold text-[#2d2a26] font-serif">
