308c357361120a0547e66b04ba674500f7430756
dndangg <144980612+dndangg@users.noreply.github.com>
2025-02-13 22:17:54 -0600

    Changed background color of login page

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -13,7 +13,7 @@ const Login: React.FC = () => {
   return (
     <div className="bg-[#808977] flex items-center justify-center min-h-screen">
       {/* Login Box */}
-      <div className="bg-[#f3e6d5] p-16 rounded-lg shadow-lg w-[95%] max-w-4xl border border-[#554f42]">
+      <div className="bg-[#e4dbcf] p-16 rounded-lg shadow-lg w-[95%] max-w-4xl border border-[#554f42]">
         <h1 className="text-center text-3xl font-bold mb-6 text-[#2d2a26] font-serif">
           Welcome to Volunteering!
         </h1>
