086316a84bf7c226e00e2649761707d6384dcb63
staybebe <sanaakhtar5674@gmail.com>
2025-03-07 14:35:13 -0600

    eventManagement API module done

--- src/pages/profileManagement.js
+++ src/pages/profileManagement.js
@@ -15,7 +15,6 @@ export default function ProfileManagement() {
   });
 
   const [errors, setErrors] = useState({});
-
   const handleChange = (e) => {
     const { name, value } = e.target;
     setFormData({ ...formData, [name]: value });
