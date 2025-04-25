e6b70f91c4404d92a2085b87d5752386d4c1b3dd
dndangg <144980612+dndangg@users.noreply.github.com>
2025-04-25 03:19:53 -0500

    reverted supabaseClient.js for security

--- planningNotes.md
+++
@@ -1,5 +0,0 @@
-## Plans
-
-1. Remove Login button from navigation bar while user is logged in
-2. Remove Sign Out button from navigation bar while user is logged out
-3. Other stuff
--- src/lib/supabaseClient.js
+++ src/lib/supabaseClient.js
@@ -1,12 +1,11 @@
-import { createClient } from "@supabase/supabase-js";
+import { createClient } from '@supabase/supabase-js';
 
-console.log("USING UPDATED supabaseClient.js");
-
-const supabaseUrl = "https://enpbjngsvjbzywitdxvh.supabase.co";
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVucGJqbmdzdmpienl3aXRkeHZoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDI0NDc3MDEsImV4cCI6MjA1ODAyMzcwMX0.SzJE0c9TArH_bBf5shXkSc2DN-5LZEIsceVjyEHLlAw";
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || '';
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || '';
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
 
-
-
-
+// Create a file in the root directory named .env.local and add the following to it:
+// NEXT_PUBLIC_SUPABASE_URL=the url under Project URL
+// NEXT_PUBLIC_SUPABASE_ANON_KEY=the anon public key
+// You can find these in the Project Settings -> Data API pages in Supabase
